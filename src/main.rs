039751a39828245984b01cfba4f9fcbac4238346
Christopher Westra <bestra@Christophers-MacBook-Pro.local>
2016-12-11 08:39:48 -0500

    Walk commit ids

--- src/main.rs
+++ src/main.rs
@@ -3,14 +3,22 @@ use git2::{Repository, Error, BranchType};
 
 fn main() {
     println!("Hello, world!");
+    open_repo(".");
 }
 
 fn open_repo(path: &str) -> Result<(), Error> {
     let repo = try!(Repository::open(path));
     let branch = try!(repo.find_branch("master", BranchType::Local));
+    let r = branch.get().target().unwrap();
 
     let mut revwalk = try!(repo.revwalk());
     revwalk.set_sorting(git2::SORT_TOPOLOGICAL);
+    try!(revwalk.push(r));
+
+    for id in revwalk {
+        let id = try!(id);
+        println!("{}", id);
+    }
 
     Ok(())
 }
